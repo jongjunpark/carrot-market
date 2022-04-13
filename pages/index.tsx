f2716f028cf39a5dccb2a2ab9ed2e103d46f9310
jongjunpark <poiufgin7373@naver.com>
2022-04-13 14:08:29 +0900

    add base ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,42 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import FloatingButton from "../components/floating-button";
+import Item from "../components/item";
+import Layout from "../components/layout";
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-slate-400 py-20 px-10 flex flex-col gap-10'>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'>
-        <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between">
-          <span className='text-gray-500'>Grey Chair</span>
-          <span>$19</span>
-        </div>
+    <Layout title="홈" hasTabBar>
+      <div className="flex flex-col space-y-5 divide-y">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item
+            id={i}
+            key={i}
+            title="iPhone 14"
+            price={99}
+            comments={1}
+            hearts={1}
+          />
+        ))}
+        <FloatingButton href="/items/upload">
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </FloatingButton>
       </div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
-    </div>
-  )
-}
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
